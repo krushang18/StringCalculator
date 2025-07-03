109a44fff38ff900b7ebd9128dee3273e6e9b6a7
krushang <krushangshah18@gmail.com>
2025-07-03 21:49:35 +0530

    Empty String

--- StringCalculator/src/com/stringcalculator/StringCalculator.java
+++ StringCalculator/src/com/stringcalculator/StringCalculator.java
@@ -1,5 +1,7 @@
 package com.stringcalculator;
 
 public class StringCalculator {
-
+	public int add(String s) {
+		return 0;
+	}
 }
--- StringCalculator/src/com/stringcalculator/StringCalculatorTest.java
+++ StringCalculator/src/com/stringcalculator/StringCalculatorTest.java
@@ -6,14 +6,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class StringCalculatorTest {
+	StringCalculator StringCalc;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		StringCalc = new StringCalculator();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testEmpty() {
+		assertEquals(0, StringCalc.add(""));
 	}
 
 }
